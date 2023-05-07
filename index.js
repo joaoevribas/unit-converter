1c55787868305aea7a72c59a0b108a61612d028d
João Ribeiro <102440368+joaoevribas@users.noreply.github.com>
2023-05-07 18:31:34 +0100

    Add js

--- index.js
+++ index.js
@@ -6,9 +6,13 @@
 1 kilogram = 2.204 pound
 */
 
-const inputBox = document.getElementById("input-box").value;
+const inputBox = document.getElementById("input-box");
 const btn = document.getElementById("btn");
+const length = document.querySelector(".length");
+const volume = document.querySelector(".volume");
+const mass = document.querySelector(".mass");
 
+// FUNCTIONS TO CONVERT UNITS
 function meterToFeet(x) {
   let result;
   result = x * 3.281;
@@ -44,3 +48,33 @@ function PoundToKilo(x) {
   result = x / 2.204;
   return result.toFixed(3);
 }
+
+// PARAGRAPH FUNCTIONS
+function lengthParagraph() {
+  return `<p>${inputBox.value} meters = ${meterToFeet(inputBox.value)} feet | ${
+    inputBox.value
+  } feet = ${feetToMeter(inputBox.value)} meters</p`;
+}
+
+function volumeParagraph() {
+  return `<p>${inputBox.value} liters = ${literToGallon(
+    inputBox.value
+  )} gallons | ${inputBox.value} gallons = ${gallonToLiter(
+    inputBox.value
+  )} liters</p`;
+}
+
+function massParagraph() {
+  return `<p>${inputBox.value} kilos = ${kiloToPound(
+    inputBox.value
+  )} pounds | ${inputBox.value} pounds = ${PoundToKilo(
+    inputBox.value
+  )} kilos</p`;
+}
+
+// BUTTON
+btn.addEventListener("click", function () {
+  length.innerHTML = lengthParagraph();
+  volume.innerHTML = volumeParagraph();
+  mass.innerHTML = massParagraph();
+});
