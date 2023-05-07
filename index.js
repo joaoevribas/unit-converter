f6de5ab7ec727c09f38c039a9bc9d6c022549743
João Ribeiro <102440368+joaoevribas@users.noreply.github.com>
2023-05-07 17:55:06 +0100

    Add functions to convert between units

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const inputBox = document.getElementById("input-box").value;
+const btn = document.getElementById("btn");
+
+function meterToFeet(x) {
+  let result;
+  result = x * 3.281;
+  return result.toFixed(3);
+}
+
+function feetToMeter(x) {
+  let result;
+  result = x / 3.281;
+  return result.toFixed(3);
+}
+
+function literToGallon(x) {
+  let result;
+  result = x * 0.264;
+  return result.toFixed(3);
+}
+
+function gallonToLiter(x) {
+  let result;
+  result = x / 0.264;
+  return result.toFixed(3);
+}
+
+function kiloToPound(x) {
+  let result;
+  result = x * 2.204;
+  return result.toFixed(3);
+}
+
+function PoundToKilo(x) {
+  let result;
+  result = x / 2.204;
+  return result.toFixed(3);
+}
